2fc8cbc73f28901b3f885e613e61880933a6a5f2
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2022-02-03 20:36:00 +0100

    added new callback

---
+++ tf_gen_models/callbacks/CheckpointPrinter.py
@@ -0,0 +1,28 @@
+import os
+from datetime import datetime
+from tensorflow.keras.callbacks import Callback
+
+
+class CheckpointPrinter (Callback):
+  def __init__ (self, step = 1) -> None:
+    super().__init__()
+    self._step = step
+
+  def on_train_begin (self, logs = None):
+    self._start = datetime.now()
+    print (f"{self._start} | Begin of the training procedure")
+
+  def on_train_end (self, logs = None):
+    stop = datetime.now()
+    timestamp = str(stop - self._start) . split (".") [0]   # HH:MM:SS
+    timestamp = timestamp . split (":")   # [HH, MM, SS]
+    timestamp = f"{timestamp[0]}h {timestamp[1]}min {timestamp[2]}s"
+    print (f"{stop} | End of the training procedure | Duration: {timestamp}")
+
+  def on_epoch_end (self, epoch, logs = None) -> None:
+    if (epoch + 1) % self._step == 0:
+      scores = ""
+      for item in logs.items():
+        scores += f"{item[0]}: {item[1]:.2e} - "
+      print (f"{datetime.now()} | Epoch: {epoch+1} | {scores[:-3]} |")
+      
--- tf_gen_models/callbacks/__init__.py
+++ tf_gen_models/callbacks/__init__.py
@@ -1,3 +1,4 @@
-from .schedulers import GanExpLrScheduler, GanPowLrScheduler
-from .ImageSaver import ImageSaver
-from .ModelSaver import ModelSaver
+from .schedulers        import GanExpLrScheduler, GanPowLrScheduler
+from .ImageSaver        import ImageSaver
+from .ModelSaver        import ModelSaver
+from .CheckpointPrinter import CheckpointPrinter
