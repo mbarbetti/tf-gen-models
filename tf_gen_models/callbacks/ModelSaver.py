dd65a5d699354e163e105f2f3e2dc3674186193b
Matteo Barbetti <matteo.barbetti.94@gmail.com>
2022-03-19 21:22:34 +0100

    added model saving at the end of the training

--- tf_gen_models/callbacks/ModelSaver.py
+++ tf_gen_models/callbacks/ModelSaver.py
@@ -3,7 +3,7 @@ from tensorflow.keras.callbacks import Callback
 
 
 class ModelSaver (Callback):
-  def __init__ (self, name, dirname = "./models", saved_model = "both", step = 1) -> None:
+  def __init__ (self, name, dirname = "./models", saved_model = "both", step = None) -> None:
     super().__init__()
     self._filename = f"{dirname}/{name}"
 
@@ -16,12 +16,22 @@ class ModelSaver (Callback):
     self._saved_model = saved_model
     self._step = step
 
+  def on_train_end (self, logs = None) -> None:
+    if self._saved_model == "gen":
+      self.model.generator . save ( f"{self._filename}/saved_gen_latest", save_format = "tf" )
+    elif self._saved_model == "disc":
+      self.model.discriminator . save ( f"{self._filename}/saved_disc_latest", save_format = "tf" )
+    else:
+      self.model.generator . save ( f"{self._filename}/saved_gen_latest", save_format = "tf" )
+      self.model.discriminator . save ( f"{self._filename}/saved_disc_latest", save_format = "tf" )    
+
   def on_epoch_end (self, epoch, logs = None) -> None:
-    if (epoch + 1) % self._step == 0:
-      if self._saved_model == "gen":
-        self.model.generator . save ( f"{self._filename}/saved_gen_ep{epoch+1:04d}", save_format = "tf" )
-      elif self._saved_model == "disc":
-        self.model.discriminator . save ( f"{self._filename}/saved_disc_ep{epoch+1:04d}", save_format = "tf" )
-      else:
-        self.model.generator . save ( f"{self._filename}/saved_gen_ep{epoch+1:04d}", save_format = "tf" )
-        self.model.discriminator . save ( f"{self._filename}/saved_disc_ep{epoch+1:04d}", save_format = "tf" )
+    if self._step is not None:
+      if (epoch + 1) % self._step == 0:
+        if self._saved_model == "gen":
+          self.model.generator . save ( f"{self._filename}/saved_gen_ep{epoch+1:04d}", save_format = "tf" )
+        elif self._saved_model == "disc":
+          self.model.discriminator . save ( f"{self._filename}/saved_disc_ep{epoch+1:04d}", save_format = "tf" )
+        else:
+          self.model.generator . save ( f"{self._filename}/saved_gen_ep{epoch+1:04d}", save_format = "tf" )
+          self.model.discriminator . save ( f"{self._filename}/saved_disc_ep{epoch+1:04d}", save_format = "tf" )
